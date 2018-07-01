185900ba8d8f15dd9048eda66b17c4ff7a49b941
andriysoroka <dree0103@gmail.com>
2018-07-02 00:56:18 +0300

    some

---
+++ Users.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: String,
+    age: Number,
+    date: { type: Date, default: Date.now }
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const User = require('./Users')
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const list = [];
+
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -8,8 +13,23 @@ app.use(bodyParser.json());
 
 app.post('/user', (req, res, next) => next(),
     (req, res) => {
-        console.log('body: ', req.body[0].name)
+        console.log('body: ', req.body)
         res.send('user created')
+
+        const user = new User({
+            name: req.body.name,
+            age: req.body.age
+        })
+
+        list.push(user);
+
+        console.log(list);
+
+        // user.save().than(user => {
+        //     res.send('user created!')
+        // }).catch(err => {
+        //     res.send(err);
+        // })
     }
 )
 
